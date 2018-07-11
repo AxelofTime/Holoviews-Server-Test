f5f9035602c4b3f444c84238063ab1661359c0fc
Christina Lau <39237359+AxelofTime@users.noreply.github.com>
2018-07-11 15:21:42 -0700

    Update fake_ipm2_working.py

--- fake_ipm2_working.py
+++ fake_ipm2_working.py
@@ -6,35 +6,63 @@ import pandas as pd
 
 import sys
 from bokeh.io import show
-from bokeh.layouts import layout
+from bokeh.layouts import layout, widgetbox
 from bokeh.models import Button
 from bokeh.plotting import curdoc
 from bokeh.io import output_file, save, export_png
 import tables
 from fake_beam import FakeBeam
+from functools import partial
 
 renderer = hv.renderer('bokeh').instance(mode='server')
 
 # Create the HexTiles plot
 def genHex(df):
-    return hv.HexTiles(pd.DataFrame(df))
+    # Get bounds for graph
+    global checkList
+    colNames = list(df)
+    lowX = df[colNames[0]].quantile(0.01)
+    highX = df[colNames[0]].quantile(0.99)
+    lowY = df[colNames[1]].quantile(0.01)
+    highY = df[colNames[1]].quantile(0.99)
+    
+    if len(checkList.index) > len(df.index): 
+        print("FAIL")
+        
+    checkList = df.copy()
+    #print(df.index)
+    
+    return hv.HexTiles(df, group="Number of events: " + str(len(df.index))).redim.range(
+        ebeam=(lowX, highX), 
+        ipm2 = (lowY, highY)).opts(
+        norm=dict(framewise=True))
 
-stream = hv.streams.Stream.define('df', df=pd.DataFrame({'ebeam':[], 'ipm2':[]}))()
+
+stream = hv.streams.Stream.define(
+    'df', df=pd.DataFrame({
+        'ebeam':[], 'ipm2':[]
+    }))()
+
 dmap = hv.DynamicMap(
     genHex,
-    streams=[stream]).options(
-    cmap = "fire",
-    bgcolor = "white").redim.range(
-    ebeam = (14420., 14450.),
-    ipm2 = (0, 1.1)).opts(
-    norm=dict(framewise=True))
+    streams=[stream])
 
 # Get data from ophyd device
 data = FakeBeam()
 ipm2List = []
 ebeamList = []
+# ipm2TimeStamp = []
+# ebeamTimeStamp = []
+
+checkList = pd.DataFrame({'ebeam':ebeamList, 'ipm2':ipm2List})
 
 # Unsure why, but there's usually a brief flash of points at the beginning. Need to fix?
+
+# def new_data(*args, **kwargs):
+    
+#     kwargs['in_value'].append(kwargs['value'])
+#     kwargs['in_time'].append(kwargs['timestamp'])
+    
 def new_data(*args, **kwargs):
     global ipm2List, ebeamList
     ipm2 = data.fake_ipm2.get()
@@ -42,8 +70,17 @@ def new_data(*args, **kwargs):
     ipm2List.append(ipm2)
     ebeamList.append(ebeam)
     # print(len(ipm2List))
+
     
-data.fake_ipm2.subscribe(new_data)
+data.fake_L3.subscribe(new_data)
+    
+# data.fake_ipm2.subscribe(
+#     partial(new_data, in_value=ipm2List, in_time=ipm2TimeStamp)
+# )
+
+# data.fake_L3.subscribe(
+#     partial(new_data, in_value=ebeamList, in_time=ebeamTimeStamp)
+# )
 
 def modify_doc(doc):
     # Create HoloViews plot and attach the document
@@ -52,39 +89,45 @@ def modify_doc(doc):
     # Stream data into plot
     def fake_tick():
         global ebeamList, ipm2List
-        print(ebeamList)
-        data = pd.DataFrame({'ebeam':ebeamList, 'ipm2':ipm2List})
+        data = pd.DataFrame({
+            'ebeam':ebeamList, 'ipm2':ipm2List
+        })
         stream.event(df=data)
    
     callback_id = None
 
     # Give button functions
-    def animate():
+    def updateGraph():
         global callback_id
-        if button.label == '► Play':
-            button.label = '❚❚ Pause'
-            callback_id = doc.add_periodic_callback(fake_tick, 50)
+        if startButton.label == '► Play':
+            startButton.label = '❚❚ Pause'
+            callback_id = doc.add_periodic_callback(fake_tick, 1000)
         else:
-            button.label = '► Play'
+            startButton.label = '► Play'
             doc.remove_periodic_callback(callback_id)
             
-            # Write data to csv file
-            # data = pd.DataFrame({'ebeam':ebeamL3Data[:counter+100], 'ipm2':ipm2Data[:counter+100]})
-            # data.to_csv('data.csv')
-            # stream2.event(df=data)
+    def saveFile():
+        # Write data to csv file
+        dataFile = pd.DataFrame({'ebeam':ebeamList, 'ipm2':ipm2List})
+        dataFile.to_csv('data.csv')
+        
+        print(len(ebeamList))
+        print("Saved!")
             
-            
-    button = Button(label='► Play', width=60)
-    button.on_click(animate)
+    # clear, start, save buttons        
+    startButton = Button(label='► Play', width=60)
+    startButton.on_click(updateGraph)
+    
+    saveButton = Button(label='Save', width=60)
+    saveButton.on_click(saveFile)
     
     # Combine the holoviews plot and widgets in a layout
     plot = layout([
     [hvplot.state],
-    [button]], sizing_mode='fixed')
+    widgetbox([startButton, saveButton])], sizing_mode='fixed')
     
     doc.add_root(plot)
     return doc
 
 # To display in a script
 modify_doc(curdoc()) 
-
