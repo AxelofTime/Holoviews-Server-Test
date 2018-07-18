aa80d1424af66f9cad2533ed43baf5eca2d3ed79
Christina Lau <39237359+AxelofTime@users.noreply.github.com>
2018-07-18 15:41:37 -0700

    Update fake_beam.py

--- fake_beam.py
+++ fake_beam.py
@@ -2,9 +2,14 @@ from ophyd.device import Device, Component as Cpt
 from ophyd.signal import EpicsSignalRO, AttributeSignal
 
 class FakeBeam(Device):
-    fake_ipm2 = Cpt(EpicsSignalRO, 'XPP:VARS:FLOAT:01')
-    fake_ipm3 = Cpt(EpicsSignalRO, 'XPP:VARS:FLOAT:02')
-    fake_L3 = Cpt(EpicsSignalRO, 'XPP:VARS:FLOAT:03')
+    # Event building does not work with the XPP:VARS:FLOAT: 
+#     fake_ipm2 = Cpt(EpicsSignalRO, 'XPP:VARS:FLOAT:01')
+#     fake_ipm3 = Cpt(EpicsSignalRO, 'XPP:VARS:FLOAT:02')
+#     fake_L3 = Cpt(EpicsSignalRO, 'XPP:VARS:FLOAT:03')
+
+    fake_ipm2 = Cpt(EpicsSignalRO, 'HX2:SB1:BMMON:_peakA_8')
+    fake_ipm3 = Cpt(EpicsSignalRO, 'HX2:SB1:BMMON:_peakA_9')
+    fake_L3 = Cpt(EpicsSignalRO, 'HX2:SB1:BMMON:_peakA_10')
     
     def __init__(self, prefix='', name='fake_beam', **kwargs):
         super().__init__(prefix=prefix, name=name, **kwargs)
@@ -12,5 +17,4 @@ class FakeBeam(Device):
     @property
     def hints(self):
         return {'fields': [self.mj.name]}
-    
-# Need to write subscribe function
+   
