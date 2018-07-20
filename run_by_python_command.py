5ccb78c314d7f4fc37f33c77be5adfe671838dac
Christina Lau <39237359+AxelofTime@users.noreply.github.com>
2018-07-20 13:17:47 -0700

    Update run_by_python_command.py
    And now with documentation!

--- run_by_python_command.py
+++ run_by_python_command.py
@@ -7,28 +7,34 @@ from bokeh.io import show
 from bokeh.layouts import layout, widgetbox, row
 from bokeh.models import Button, Slider, Select, HoverTool
 from bokeh.plotting import curdoc
-from bokeh.io import output_file, save, export_png
+from bokeh.io import output_file, save
 from bokeh.server.server import Server
 from bokeh.application import Application
-from bokeh.application.handlers.function import FunctionHandler
 import tables
 from fake_beam import FakeBeam
 from functools import partial
 from collections import deque
 from event_builder import basic_event_builder
-from threading import Lock
-import logging 
 
 renderer = hv.renderer('bokeh').instance(mode='server')
 
-# Create contour plot
 def gen_background(df):
+    """
+    Return holoviews contour plot based on image generated from data
+    
+    Parameters
+    ----------
+    
+    df: pandas.DataFrame
+        DataFrame containing data to be ploted on contour plot
+        
+    """
     
     colNames = list(df)
     ebeamL3 = df[colNames[0]].astype(float)
     ipmValue = df[colNames[1]].astype(float)
    
-    #Reverse xbins array to provide the correct y-axis 
+    #Reverse xbins array to provide the correct y-axis (otherwise it's flipped)
     xbinsReversed = np.linspace(np.percentile(ipmValue, 1), np.percentile(ipmValue, 99),30)
     xbins = xbinsReversed[::-1]
     ybins = np.linspace(np.percentile(ebeamL3, 1), np.percentile(ebeamL3, 99), 30)
@@ -40,22 +46,31 @@ def gen_background(df):
     high = np.percentile(iSig, 99)
     
     # Changing axis names so they don't match HexTiles names
-    x = colNames[0] + " testing"
-    y = colNames[1] + " testing"
+    x = colNames[0] + " Contour"
+    y = colNames[1] + " Contour"
     
     img_less_bins = hv.Image(
-        iSig, 
-        bounds=(ybins[0], xbins[0], ybins[-1], xbins[-1]), 
-        kdims=[x, y]).options(
-        show_legend=False).redim.range(z=(low, high))
+        iSig, bounds=(ybins[0], xbins[0], ybins[-1], xbins[-1]), kdims=[x, y]).options(
+        show_legend=False).redim.range(
+        z=(low, high))
     
     return hv.operation.contours(
-        img_less_bins, 
-        filled=False, group="General Contour Plot").options(
+        img_less_bins, filled=False, group="General Contour Plot").options(
         cmap='fire').opts(
         norm=dict(framewise=True))
 
 def gen_hex(df):
+    """
+    Return holoviews HexTiles plot
+    
+    Parameters
+    ----------
+    
+    df: pandas.DataFrame
+        DataFrame containing data to be ploted on hextiles plot
+    
+    """
+    
     # Get bounds for graph
     colNames = list(df)
     lowX = df[colNames[0]].quantile(0.01)
@@ -68,11 +83,52 @@ def gen_hex(df):
         ipm2 = (lowY, highY)).opts(
         norm=dict(framewise=True))
 
-# Make scatter plot
 def gen_scatter(df):
+    """
+    Return holoviews scatter plot
+    
+    Parameters
+    ----------
+    
+    df: pandas.DataFrame
+        DataFrame containing data to be ploted on scatter plot
+    
+    """
     return hv.Scatter(df).options(size=10, tools=['hover'], apply_ranges=False)
 
-def produce_hex(doc, ipm2List, ipm3List, ebeamList, ipm2TS, ipm3TS, ebeamTS):    
+def produce_hex(doc, ipm2List, ipm3List, ebeamList, ipm2TS, ipm3TS, ebeamTS): 
+    """
+    Produce updating hextiles plot and push them onto the web page document.
+    User may save current data, clear existing data, or pause the graph. User
+    can also switch between ipm2 and ipm3 over ebeam graphs.
+    
+    Parameters
+    ----------
+    
+    doc: bokeh.document (I think)
+        Bokeh document to be displayed on webpage
+    
+    ipm2List: deque
+        Deque containing updating ipm2 values
+        
+    ipm3List: deque
+        Deque containing updating ipm3 values
+    
+    ebeamList: deque
+        Deque containing updating ebeam values
+    
+    ipm2TS: deque
+        Deque containing updating ipm2 timestamps
+    
+    ipm3TS: deque
+        Deque containing updating ipm3 timestamps
+    
+    ebeamTS: deque
+        Deque containing updating ebeam timestamps
+    
+    """
+    
+    # Create copy of deques for each instance of server
     ipm2_plot = list(ipm2List)
     ipm3_plot = list(ipm3List)
     ebeam_plot = list(ebeamList)
@@ -93,12 +149,22 @@ def produce_hex(doc, ipm2List, ipm3List, ebeamList, ipm2TS, ipm3TS, ebeamTS):
         gen_hex,
         streams=[streamHex])
     
+    # Use bokeh to render plot
     hvplot = renderer.get_plot(plot, doc)
     
-    callback_id_scatter = None
+    # Initialize callback id for hextiles plot
+    callback_id_hex = None
+    
+    # Initialize values that can be updated by widgets
     switch_key_hex = 'ipm2'
     
     def clear():
+        """
+        "Clear" graphs and particular lists of server instance. Save current index
+        and only plot points after that index.
+        
+        """
+        
         nonlocal ipm2_index, ipm3_index, ebeam_index, ipm2TS_index, ipm3TS_index, ebeamTS_index
         
         ipm2_index = len(ipm2List)
@@ -109,6 +175,11 @@ def produce_hex(doc, ipm2List, ipm3List, ebeamList, ipm2TS, ipm3TS, ebeamTS):
         ebeamTS_index = len(ebeamTS)
         
     def push_data():
+        """
+        Push data into stream to be ploted on hextiles plot
+        
+        """
+        
         nonlocal ipm2_plot, ipm3_plot, ebeam_plot, ipm2TS_plot, ipm3TS_plot, ebeamTS_plot
         
         ipm2_plot = list(ipm2List)[ipm2_index:]
@@ -123,30 +194,47 @@ def produce_hex(doc, ipm2List, ipm3List, ebeamList, ipm2TS, ipm3TS, ebeamTS):
         ebeamData = pd.Series(ebeam_plot, index=ebeamTS_plot)
         zipped = basic_event_builder(ipm2=ipm2Data, ipm3=ipm3Data, ebeam=ebeamData)
         data = zipped[['ebeam', switch_key_hex]]
+        #print(zipped)
         streamHex.event(df=data)
     
     def play_graph():
-        nonlocal callback_id_scatter
+        """
+        Provide play and pause functionality to the graph
+
+        """
+        
+        nonlocal callback_id_hex
         if startButton.label == '► Play':
             startButton.label = '❚❚ Pause'
             callback_id_scatter = doc.add_periodic_callback(push_data, 1000)
         else:
             startButton.label = '► Play'
-            doc.remove_periodic_callback(callback_id_scatter)
+            doc.remove_periodic_callback(callback_id_hex)
             
-    # Write data to csv file
     def saveFile():
+        """
+        Save current data plotted to csv file as a pandas.DataFrame
+        
+        """
+        
         ipm2Data = pd.Series(ipm2_plot, index=ipm2TS_plot)
         ipm3Data = pd.Series(ipm3_plot, index=ipm3TS_plot)
         ebeamData = pd.Series(ebeam_plot, index=ebeamTS_plot)
         zipped = basic_event_builder(ipm2=ipm2Data, ipm3=ipm3Data, ebeam=ebeamData)
         zipped.to_csv('data2.csv')
     
-    # Add function to switch while paused as well        
+    # Need to add function to switch while paused as well
+    
     def switch(attr, old, new):
+        """
+        Switch hextiles plot when drop down menu value is updated
+        
+        """
+        
         nonlocal switch_key_hex
         switch_key_hex = select.value
     
+    # Create widgets
     clearButton = Button(label='Clear')
     clearButton.on_click(clear)
     
@@ -159,6 +247,7 @@ def produce_hex(doc, ipm2List, ipm3List, ebeamList, ipm2TS, ipm3TS, ebeamTS):
     startButton = Button(label='► Play')
     startButton.on_click(play_graph)
     
+    # Layout
     row_buttons = row([widgetbox([startButton, clearButton, saveButton], sizing_mode='stretch_both')])
     
     plot = layout([[hvplot.state], 
@@ -167,14 +256,46 @@ def produce_hex(doc, ipm2List, ipm3List, ebeamList, ipm2TS, ipm3TS, ebeamTS):
                        
     #plot = layout([[hvplot.state],widgetbox([startButton, select, clearButton, saveButton])], sizing_mode='fixed')
     
+    doc.title = "Hextiles Graph"
     doc.add_root(plot)
     
 def produce_scatter_on_background(doc, ipm2List, ipm3List, ebeamList, ipm2TS, ipm3TS, ebeamTS):
+    """
+    Produce background plot with updating scatter plot on top of it 
+    and push them onto the web page document. User can control how many
+    points of the scatter plot appears and update the contour plot. User
+    may also switch between ipm2 and ipm3 over ebeam graphs.
     
-    # Interesting error, background doesn't seem to update after running multiple instances to the latest version and starts
-    # off with the data the first instance was given. Need to figure out why!
+    Parameters
+    ----------
     
-    # Still has the freezing points error, though, it seems to come more frequently. This may be a bigger problem now
+    doc: bokeh.document (I think)
+        Bokeh document to be displayed on webpage
+    
+    ipm2List: deque
+        Deque containing updating ipm2 values
+        
+    ipm3List: deque
+        Deque containing updating ipm3 values
+    
+    ebeamList: deque
+        Deque containing updating ebeam values
+    
+    ipm2TS: deque
+        Deque containing updating ipm2 timestamps
+    
+    ipm3TS: deque
+        Deque containing updating ipm3 timestamps
+    
+    ebeamTS: deque
+        Deque containing updating ebeam timestamps
+    
+    """
+    
+    # Interesting error, background doesn't seem to update after running multiple instances 
+    # to the latest version and starts off with the data the first instance was given. 
+    # Need to figure out why!
+    
     curBackData = pd.read_csv('data2.csv', index_col='Unnamed: 0')
     
     # Streams
@@ -197,13 +318,24 @@ def produce_scatter_on_background(doc, ipm2List, ipm3List, ebeamList, ipm2TS, ip
     
     overlay = (dmapBackground*dmapScatter).options(show_legend=False).opts(norm=dict(axiswise=True))
     
+    # Render plot with bokeh
     hvplot = renderer.get_plot(overlay, doc)
     
-    callback_id2 = None
+    # Initialize callback for scatterplot
+    callback_id_scatter = None
+    
+    # Initialize variables than can be updated by widgets
     switch_key_scatter = 'ipm2'
     limit = 50
     
     def scatter_tick():
+        """
+        Push new scatter points into stream to plot.
+        
+        """
+        
+        # Still has the freezing points error, though, it seems to come more frequently. 
+        # This may be a bigger problem now
         
         ebeamConverted = list(ebeamList)
         ipm2Converted = list(ipm2List)
@@ -215,6 +347,7 @@ def produce_scatter_on_background(doc, ipm2List, ipm3List, ebeamList, ipm2TS, ip
         ipm2Data = pd.Series(ipm2Converted[-limit:], index=ipm2TimeConverted[-limit:])
         ipm3Data = pd.Series(ipm3Converted[-limit:], index=ipm3TimeConverted[-limit:])
         ebeamData = pd.Series(ebeamConverted[-limit:], index=ebeamTimeConverted[-limit:])
+        
         zipped = basic_event_builder(ipm2=ipm2Data, ipm3=ipm3Data, ebeam=ebeamData)
 
         scatterList = zipped[-limit:]
@@ -224,19 +357,38 @@ def produce_scatter_on_background(doc, ipm2List, ipm3List, ebeamList, ipm2TS, ip
         streamScatter.event(df=data)
     
     def limit_update(attr, old, new):
+        """
+        Update limit slider value
+        
+        """
         nonlocal limit
         limit = limitSlider.value
     
     def switch(attr, old, new):
+        """
+        Update drop down menu value
+        
+        """
+        
         nonlocal switch_key_scatter
         switch_key_scatter = select.value
     
     def switch_background(attr, old, new):
+        """
+        Switch background when drop down menu value is updated
+        
+        """
+        
         nonlocal curBackData
         data = curBackData[['ebeam', switch_key_scatter]]
         streamContour.event(df=data)
         
     def update_background():
+        """
+        Update background with most updated data when update button is pressed
+        
+        """
+        
         nonlocal curBackData
         newData = pd.read_csv('data2.csv', index_col='Unnamed: 0')
         curBackData = newData
@@ -244,8 +396,10 @@ def produce_scatter_on_background(doc, ipm2List, ipm3List, ebeamList, ipm2TS, ip
         
         streamContour.event(df=data)
     
-    callback_id2 = doc.add_periodic_callback(scatter_tick, 500)
+    # Continuously update scatter plot
+    callback_id_scatter = doc.add_periodic_callback(scatter_tick, 500)
     
+    # Create widgets
     limitSlider = Slider(start=10, end=1000, value=50, step=1, title="Number of Events")
     limitSlider.on_change('value', limit_update)
     
@@ -259,24 +413,39 @@ def produce_scatter_on_background(doc, ipm2List, ipm3List, ebeamList, ipm2TS, ip
     plot = layout([[hvplot.state], 
                    widgetbox([limitSlider, select, updateButton], sizing_mode='stretch_both')])
     
+    doc.title = "Reference Graph"
     doc.add_root(plot)
 
 def new_data(*args, **kwargs):
+    """
+    Append data from subscribe into data containers
+    
+    """
     
     kwargs['in_value'].append(kwargs['value'])
     kwargs['in_time'].append(kwargs['timestamp'])
 
 def launch_server():
+    '''
+    Launch a bokeh_server to plot a hextiles plot of ipm value over ebeam value
+    and generate a background contour plot with updating scatter plot on top of it.
+    Functionalities include a save button, clear button, pause button, and (buggy) ipm
+    select drop down menu for the hextiles plot. Contour and scatter plot have an update
+    button and select drop down.
+    
+    '''
+    maxlen = 100000
     
     # Get data
     beam = FakeBeam()
-    ipm2List = deque(maxlen=100000)
-    ipm3List = deque(maxlen=100000)
-    ebeamList = deque(maxlen=100000)
-    ipm2TimeStamp = deque(maxlen=100000)
-    ipm3TimeStamp = deque(maxlen=100000)
-    ebeamTimeStamp = deque(maxlen=100000)
+    ipm2List = deque(maxlen=maxlen)
+    ipm3List = deque(maxlen=maxlen)
+    ebeamList = deque(maxlen=maxlen)
+    ipm2TimeStamp = deque(maxlen=maxlen)
+    ipm3TimeStamp = deque(maxlen=maxlen)
+    ebeamTimeStamp = deque(maxlen=maxlen)
     
+    # Subscribe to devices
     beam.fake_ipm2.subscribe(
         partial(new_data, in_value=ipm2List, in_time=ipm2TimeStamp)
     )
@@ -319,6 +488,10 @@ def launch_server():
         num_procs=1,
     )
     server.start()
+    
+    print('Opening Bokeh application on:')
+    for entry in origins:
+        print('\thttp://{}/'.format(entry))
     
     try:
         server.io_loop.start()
